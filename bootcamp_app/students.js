a1e4a891e4afdb09ea0bc17d4cfa7b97189cabff
m-vojjla <mounikavojjala@gmail.com>
2020-11-26 13:34:06 +0000

    sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,16 @@ const pool = new Pool({
 
 pool
     .connect()
-    .then((client) => console.log(`Connected to ${pool.database} db on ${pool.host}`))
-    .catch((err) => console.log(`Error connecting to ${database} db`));
+    .then((client) => console.log(`Connected to ${client.database} db on ${client.host}`))
+    .catch((err) => console.log(`${err}`));
+    const sqlQuery = {
+      text:`SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+      FROM students JOIN cohorts ON cohorts.id = students.cohort_id WHERE cohorts.name LIKE $1
+      LIMIT $2`,
+      values:[`%${process.argv[2]}%`,process.argv[3] || 5]
+    }
 
-    pool.query(`
-    SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-    FROM students JOIN cohorts ON cohorts.id = students.cohort_id WHERE cohorts.name LIKE '%${process.argv[2]}%'
-    LIMIT ${process.argv[3] || 5};
-    `)
+    pool.query(sqlQuery)
     .then(res => {
       res.rows.forEach(user => {
         console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,23 +9,26 @@ const pool = new Pool({
 
 pool
     .connect()
-    .then((client) => console.log(`Connected to ${pool.database} db on ${pool.host}`))
-    .catch((err) => console.log(`Error connecting to ${pool.database} db`));
+    .then((client) => console.log(`Connected to ${client.database} db on ${client.host}`))
+    .catch((err) => console.log(`${err}`));
+   
+    const sqlQuery = {
+      text:  `SELECT
+      DISTINCT teachers.name AS teacher,
+      cohorts.name AS cohort
+    FROM
+      teachers
+      JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+      JOIN students ON assistance_requests.student_id = students.id
+      JOIN cohorts ON students.cohort_id = cohorts.id
+    WHERE
+      cohorts.name = $1
+    ORDER BY
+      teacher`,
+      values:[`${process.argv[2]}`]
+    }
 
-    pool.query(`
-    SELECT
-  DISTINCT teachers.name AS teacher,
-  cohorts.name AS cohort
-FROM
-  teachers
-  JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
-  JOIN students ON assistance_requests.student_id = students.id
-  JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE
-  cohorts.name = '${process.argv[2] || 'JUL02' }'
-ORDER BY
-  teacher;
-    `)
+    pool.query(sqlQuery)
     .then(res => {
       res.rows.forEach(row => {
         console.log(`${row.cohort} : ${row.teacher}`);
